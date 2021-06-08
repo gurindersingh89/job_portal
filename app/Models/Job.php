20a9c722489b07744f15b5e9b183031f2c713514
Gurinder Singh <gurindersingh.dev@gmail.com>
2021-06-08 08:28:41 +0530

    job soft delete added

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Job extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     public $fillable = ['title', 'description', 'qualification', 'no_of_openings', 'department', 'salary', 'job_status', 'created_by'];
     
